af7e41c711043eaa3c1b16273bbefbd11260c0b1
L30 <marzolileo@gmail.com>
2023-02-28 00:58:53 +0100

    Update SimpleBankAccountWithAtmTest.java
    Aggiornamento File.

---
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,57 @@
+package lab01.example.model;
+
+public class SimpleBankAccountWithAtm implements BankAccount{
+
+    private double balance;
+    private final AccountHolder holder;
+
+    /* Introduction of quality: Remove repetitive code. */
+
+    private SimpleBankAccount simpleBankAccount;
+
+    /* **** */
+
+    public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
+        this.holder = holder;
+        this.balance = balance;
+        this.simpleBankAccount = new SimpleBankAccount(this.holder, this.balance);
+    }
+    @Override
+    public AccountHolder getHolder(){
+        return this.holder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            System.out.println("Prima "+amount);
+            //TODO Fare un riferimento per delegazione con SimpleBankAccount
+            if (checkUser(userID)) {
+                this.balance += amount - 1;
+            }
+            System.out.println("Dopo " + amount);
+            //this.balance += amount;
+        }
+    }
+
+    @Override
+    public void withdraw(final int userID, final double amount) {
+        if (checkUser(userID) && isWithdrawAllowed(amount)) {
+            this.balance -= amount + 1;
+            //this.balance -= amount;
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return this.balance >= (amount + 1) /* 1$ di transazione */;
+    }
+
+    private boolean checkUser(final int id) {
+        return this.holder.getId() == id;
+    }
+}
